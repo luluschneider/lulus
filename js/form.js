0760a0870d4eb0ee138c086c79bb7c4bb50b2ea5
luluschneider <112031747+luluschneider@users.noreply.github.com>
2022-11-30 16:32:44 -0500

    Delete js directory

--- js/form.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById("form")
-const container = document.querySelector(".contact-container")
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  container.innerHTML = "<p>thank you for your message! <br /> <3 </p>"
-})
