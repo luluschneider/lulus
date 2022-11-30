42ab9b5672303149a71b05280c756bf8f74e1f19
luluschneider <112031747+luluschneider@users.noreply.github.com>
2022-11-30 16:39:06 -0500

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form")
+const container = document.querySelector(".contact-container")
+
+form.addEventListener("submit", (e) => {
+  container.innerHTML = "<p>thank you for your message! <br /> <3 </p>"
+})
